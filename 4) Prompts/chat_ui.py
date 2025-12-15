dedb90de2e1cfc4f6e52ebe6b73ed1e44e7fb978
AnirudhS3110 <anirudh23bcs19@iiitkottayam.ac.in>
2025-12-15 19:08:16 +0530

    just showing up, just created a small LLM based Summaizr/researchapp with sttreamlit

---
+++ 4) Prompts/chat_ui.py
@@ -0,0 +1,21 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+import streamlit as st
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-pro",
+    temperature=0.7
+)
+
+st.header("Research Tool")
+
+user_input = st.text_input("Enter your prompt")
+
+if st.button("Click!"):
+    if user_input and user_input.strip():
+        result = llm.invoke(user_input)
+        st.write(result.content)
+    else:
+        st.warning("Please enter a prompt")
---
+++ 4) Prompts/readme.md
@@ -0,0 +1,5 @@
+# Prompots:
+- THese are inout instructions or Queries given to a model to guide to its output
+## Two types of PRompts:
+- Static
+- Dynamic
